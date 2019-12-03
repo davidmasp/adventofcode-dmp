7968516868736aeb277bc3021c2eb82a65b9dbf6
david mas <david.mas.p@gmail.com>
2019-12-03 22:30:16 +0100

    updated, now slow but working

--- 2019/3/script.R
+++ 2019/3/script.R
@@ -3,12 +3,12 @@ library(magrittr)
 
 transform_to_path <- function(path,start) {
 
-
-  string_split = stringr::str_split(path,pattern = "")
-  values = string_split %>% purrr::map(2) %>% unlist %>% as.numeric()
+  values = stringr::str_extract(pattern = "[:digit:]+",
+                                string = path) %>% as.numeric()
   total_points = sum(values)*2
   final_idx = cumsum(values)
-  direction = string_split %>% purrr::map(1) %>% unlist()
+  direction = stringr::str_extract(pattern = "^[R|L|U|D]",
+                                   string = path)
   sign =  dplyr::case_when(
     direction == "R" ~ 1,
     direction == "L" ~ -1,
@@ -28,7 +28,7 @@ transform_to_path <- function(path,start) {
   path_m = matrix(integer(total_points),ncol = 2)
   initial_idx = 1
   cpoint = start
-  for (i in 1:length(string_split)){
+  for (i in 1:length(values)){
     initial_point = cpoint[dimension[i]] +  sign[i]
     final_points = initial_point:(initial_point + values[i] - sign[i] )
 
@@ -57,21 +57,67 @@ crossed_wires <- function(s1,s2) {
   mat1 = transform_to_path(s1,start = st_point)
   mat2 = transform_to_path(s2,start = st_point)
 
-  match = apply(mat1,1, function(x){which(apply(mat2,1, function(y){all(y == x)}))}) %>% unlist()
+  match = apply(mat1,1, function(x){
+    which(apply(mat2,1, function(y){
+      all(y == x)}))}) %>% unlist()
 
   min(apply(mat2[match,], 1, manhattan,v2 = st_point))
 
 }
 
-# test 1
+plot_paths <- function(s1,s2){
+  st_point = c(1,1)
+  
+  mat1 = transform_to_path(s1,start = st_point)
+  mat2 = transform_to_path(s2,start = st_point)
+  
+  library(ggplot2)
+  mat1 %>% as.data.frame() %>% dplyr::mutate(lbl = 1:nrow(.),type = 1) -> m1df 
+  mat2 %>% as.data.frame() %>% dplyr::mutate(lbl = 1:nrow(.),type = 2) -> m2df 
+  
+  fp = ggplot(rbind(m1df,m2df),
+         aes(x = V1,y = V2,
+             color = factor(type))) +
+    geom_text(aes(label = lbl))
+  
+  return(fp)
+}
+
+
+## EXAMPLES TEST
 
 # define the starting point
 string_1 = c("R8","U5","L5","D3")
 string_2 = c("U7","R6","D4","L4")
 
 crossed_wires(s1 = string_1,s2 = string_2)
+plot_paths(s1 = string_1,s2 = string_2)
 
-crossed_wires(s1 = c("R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"),
-              s2 = c("U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"))
+string_1 = c("R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72")
+string_2 = c("U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83")
+crossed_wires(s1 = string_1,
+              s2 = string_2)
+plot_paths(s1 = string_1,
+           s2 = string_2)
+
+string_1 = c("R98", "U47", "R26", "D63", "R33", "U87", "L62", "D20", "R33", "U53", "R51")
+string_2 = c("U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7")
+
+crossed_wires(s1 = string_1,
+              s2 = string_2)
+plot_paths(s1 = string_1,
+           s2 = string_2)
 
 
+## INPUT
+
+dat = readr::read_lines("input.txt")
+dat %>% purrr::map(stringr::str_split,
+                   pattern = ",") %>% unlist(recursive = F) -> dat_list
+
+crossed_wires(s1 = dat_list[[1]],
+              s2 = dat_list[[2]])
+
+
+plot_paths(s1 = dat_list[[1]],
+           s2 = dat_list[[2]])
